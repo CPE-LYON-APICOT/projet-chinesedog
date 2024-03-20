3fd4fe35d9b67fc813a60f5d86f7e1b30241d850
Thierry PORTAL <t.portal@eleve.leschartreux.net>
2024-03-20 16:44:38 +0100

    Switch Gradle vers Maven

--- ChineseDog/src/main/java/module-info.java
+++ ChineseDog/src/main/java/module-info.java
@@ -1,15 +1,8 @@
 module com.example.chinesedog {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
 
-    requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
 
     opens com.example.chinesedog to javafx.fxml;
     exports com.example.chinesedog;
