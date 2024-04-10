0e435f2fb368d6380cb9fa58d7bb32312669a7cb
Thierry PORTAL <t.portal@eleve.leschartreux.net>
2024-04-10 08:19:16 +0200

    test

--- ChineseDog/src/main/java/com/example/chinesedog/test.java
+++ ChineseDog/src/main/java/com/example/chinesedog/test.java
@@ -28,18 +28,18 @@ public class test extends Application {
     public void start(Stage primaryStage) throws IOException {
         // Load TMX file
         // Charger les images
-        BufferedImage image1 = ImageIO.read(new File("C:\\Users\\Portal\\Desktop\\Cours\\POO\\Projet\\projet-chinesedog\\ChineseDog\\src\\main\\resources\\com\\example\\chinesedog\\assets\\Texture\\4\\PNG\\mapTile_024.png"));
-        BufferedImage image2 = ImageIO.read(new File("C:\\Users\\Portal\\Desktop\\Cours\\POO\\Projet\\projet-chinesedog\\ChineseDog\\src\\main\\resources\\com\\example\\chinesedog\\assets\\Texture\\4\\PNG\\mapTile_027.png"));
+        BufferedImage herbe = ImageIO.read(new File("C:\\Users\\Portal\\Desktop\\Cours\\POO\\Projet\\projet-chinesedog\\ChineseDog\\src\\main\\resources\\com\\example\\chinesedog\\assets\\Texture\\4\\PNG\\mapTile_024.png"));
+        BufferedImage pierre = ImageIO.read(new File("C:\\Users\\Portal\\Desktop\\Cours\\POO\\Projet\\projet-chinesedog\\ChineseDog\\src\\main\\resources\\com\\example\\chinesedog\\assets\\Texture\\4\\PNG\\mapTile_027.png"));
 
         // Créer une nouvelle image avec une largeur et une hauteur suffisamment grandes pour contenir les deux images
-        int combinedWidth = image1.getWidth() + image2.getWidth();
-        int combinedHeight = Math.max(image1.getHeight(), image2.getHeight());
+        int combinedWidth = herbe.getWidth() + pierre.getWidth();
+        int combinedHeight = Math.max(herbe.getHeight(), pierre.getHeight());
         BufferedImage combinedImage = new BufferedImage(combinedWidth, combinedHeight, BufferedImage.TYPE_INT_ARGB);
 
         // Dessiner les deux images sur l'image combinée
         Graphics2D g2d = combinedImage.createGraphics();
-        g2d.drawImage(image1, 0, 0, null);
-        g2d.drawImage(image2, image1.getWidth(), 0, null);
+        g2d.drawImage(herbe, 0, 0, null);
+        g2d.drawImage(pierre, herbe.getWidth(), 0, null);
         g2d.dispose();
 
         // Enregistrer l'image combinée
@@ -49,10 +49,10 @@ public class test extends Application {
         System.out.println("L'image combinée a été enregistrée avec succès.");
 
         // Dessiner les deux images sur l'image combinée
-        BufferedImage graphicsImage = combinedImage.getSubimage(0, 0, image1.getWidth(), combinedHeight);
-        graphicsImage.getGraphics().drawImage(image1, 0, 0, null);
-        graphicsImage = combinedImage.getSubimage(image1.getWidth(), 0, image2.getWidth(), combinedHeight);
-        graphicsImage.getGraphics().drawImage(image2, 0, 0, null);
+        BufferedImage graphicsImage = combinedImage.getSubimage(0, 0, herbe.getWidth(), combinedHeight);
+        graphicsImage.getGraphics().drawImage(herbe, 0, 0, null);
+        graphicsImage = combinedImage.getSubimage(herbe.getWidth(), 0, pierre.getWidth(), combinedHeight);
+        graphicsImage.getGraphics().drawImage(pierre, 0, 0, null);
 
         // Convertir l'image combinée en Image JavaFX
         Image fxImage = SwingFXUtils.toFXImage(combinedImage, null);
